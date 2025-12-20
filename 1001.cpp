7c05f4764d3169ed782b0627b4de4a537cda3765
gsilver7 <kmjmath002@gmail.com>
2025-12-21 05:14:24 +0900

    생성

---
+++ 1001.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // 코드 작성
+    int a,b;
+    cin >> a >> b;
+    cout << a+b;
+    return 0;
+}
---
+++ templete.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // 코드 작성
+
+    return 0;
+}
